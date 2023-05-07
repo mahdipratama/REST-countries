e8bcf538ab87d67a8229964633b1e98615054583
mahdipratama <mahdipratama29@gmail.com>
2023-05-07 21:03:32 +0700

    implementing CountryDetail components to show the details about current country

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,24 +1,28 @@
 /* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom';
+
 const Country = ({ id, name, capital, population, region, flag, alt }) => {
   const formattedPopulation = population.toLocaleString('de-DE');
 
   return (
     <article className="country" key={id}>
-      <div className="country-img">
-        <img src={flag} alt={alt} />
-      </div>
-      <div className="country-desc">
-        <h3>{name}</h3>
-        <p>
-          Population: <span>{formattedPopulation}</span>
-        </p>
-        <p>
-          Region: <span>{region}</span>{' '}
-        </p>
-        <p>
-          Capital: <span>{capital}</span>{' '}
-        </p>
-      </div>
+      <Link to={`/country/${id}`}>
+        <div className="country-img">
+          <img src={flag} alt={alt} />
+        </div>
+        <div className="country-desc">
+          <h3>{name}</h3>
+          <p>
+            Population: <span>{formattedPopulation}</span>
+          </p>
+          <p>
+            Region: <span>{region}</span>{' '}
+          </p>
+          <p>
+            Capital: <span>{capital}</span>{' '}
+          </p>
+        </div>
+      </Link>
     </article>
   );
 };
--- src/pages/CountryDetail.jsx
+++ src/pages/CountryDetail.jsx
@@ -1,5 +1,130 @@
+import { useState, useEffect } from 'react';
+import Loading from '../components/Loading';
+import { useParams, Link } from 'react-router-dom';
+
+const countryURL = 'https://restcountries.com/v3.1/alpha/';
+
 const CountryDetail = () => {
-  return <div>CountryDetail</div>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [country, setCountry] = useState(null);
+
+  const getCountry = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${countryURL}${id}`);
+
+      if (!response.ok) throw new Error("Couldn't get Country Data");
+
+      const data = await response.json();
+
+      const dataCountry = data.map(country => {
+        const {
+          flags: { svg, alt },
+          name: { common },
+          population,
+          region,
+          subregion,
+          capital,
+          tld,
+        } = country;
+
+        const nativeKey = Object.keys(country.name.nativeName)[0];
+        const native = country.name.nativeName[nativeKey].common;
+
+        const currenciesKey = Object.keys(country.currencies)[0];
+        const currencies = country.currencies[currenciesKey].name;
+
+        const languages = Object.values(country.languages);
+
+        const borders = country.borders ?? [];
+
+        return {
+          flag: svg,
+          alt,
+          name: common,
+          native,
+          population,
+          region,
+          subregion,
+          capital,
+          tld,
+          currencies,
+          languages,
+          borders,
+        };
+      });
+
+      setCountry(dataCountry);
+
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getCountry();
+  }, [id]);
+
+  if (loading) return <Loading />;
+
+  if (!country) return <h2>no country to display</h2>;
+
+  const {
+    flag,
+    alt,
+    name,
+    native,
+    population,
+    region,
+    subregion,
+    capital,
+    tld,
+    currencies,
+    languages,
+    borders,
+  } = country[0];
+
+  return (
+    <section className="container">
+      <Link to="/">
+        <button className="back-btn">
+          <i className="fa-solid fa-arrow-left-long"></i> Back
+        </button>
+      </Link>
+      <article className="country-detail">
+        <div className="country-img-detail">
+          <img src={flag} alt={alt} />
+        </div>
+        <div className="country-desc-detail">
+          <div className="country-detail-1">
+            <h2>{name}</h2>
+            <span>Native Name: </span>
+            {native}
+            <span>Population: </span>
+            {population}
+            <span>Region: </span>
+            {region}
+            <span>Sub Region: </span>
+            {subregion}
+            <span>Capital: </span>
+            {capital}
+          </div>
+          <div className="country-detail-2">
+            <span>Top Level Domain: </span>
+            {tld}
+            <span>Currencies: </span>
+            {currencies}
+            <span>Languages: </span>
+            {languages}
+          </div>
+          <div className="country-borders">{borders}</div>
+        </div>
+      </article>
+    </section>
+  );
 };
 
 export default CountryDetail;
